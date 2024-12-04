09351d6671952ab9f448f9440a071c92608696d3
Imran Farooq <imran_farooq7@hotmail.com>
2024-12-04 20:39:38 +0500

    edit reusme link added

--- app/components/resume-card/resume-card.tsx
+++ app/components/resume-card/resume-card.tsx
@@ -1,15 +1,17 @@
 import { Resume } from "@prisma/client";
 import PersonalInfo from "../personal-info/personal-info";
+import Link from "next/link";
 
 const ResumeCard = ({ resume }: { resume: Resume }) => {
 	return (
-		<div
+		<Link
+			href={`/dashboard/resume/edit/${resume.id}`}
 			className={`shadow-lg space-y-4 w-full min-h-52 p-4 border-b-[10px] border-${[
 				resume.themeColor,
 			]}`}
 		>
 			<PersonalInfo resume={resume} />
-		</div>
+		</Link>
 	);
 };
 

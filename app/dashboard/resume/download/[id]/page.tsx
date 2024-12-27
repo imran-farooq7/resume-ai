ff3efd25aae0e39f55a9fb92ee41cafbc00d2586
Imran Farooq <imran_farooq7@hotmail.com>
2024-12-27 22:38:33 +0500

    Resume download page created

---
+++ app/dashboard/resume/download/[id]/page.tsx
@@ -0,0 +1,20 @@
+const ResumeDownload = () => {
+	return (
+		<div className="min-h-screen flex flex-col justify-center">
+			<h2>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, culpa?
+			</h2>
+			<div className="flex justify-between">
+				<button className="btn btn-success text-white min-w-20">Add</button>
+
+				<button className="btn btn-error text-white min-w-20">Remove</button>
+
+				<button type="submit" className="btn btn-success text-white min-w-20">
+					Next
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default ResumeDownload;
